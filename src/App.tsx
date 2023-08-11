95ffa269636420961c4727f06e131e4403abf79d
Farkhod Kodirov <farxod94@gmail.com>
2023-08-12 04:01:10 +0500

    feat: add action checkAuth and loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { FC } from "react";
+import { FC, useContext, useEffect } from "react";
 import LoginForm from "./components/LoginForm";
+import { Context } from "./main";
+import { observer } from "mobx-react-lite";
 import RegistrationForm from "./components/RegistrationForm";
+import UsersTable from "./components/UsersTable";
 
 const App: FC = () => {
-  return (
-    <>
-      <RegistrationForm />
-    </>
-  );
+  const { store } = useContext(Context);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      store.checkAuth();
+    }
+  }, []);
+
+  if (!store.isAuth) {
+    return (
+      <div>
+        <LoginForm />
+      </div>
+    );
+  }
+
+  return <UsersTable />;
 };
 
-export default App;
+export default observer(App);
---
+++ src/components/UsersTable.tsx
@@ -0,0 +1,7 @@
+import { Context } from "../main";
+import { observer } from "mobx-react-lite";
+const UsersTable: React.FC = () => {
+  return <>АВТОРИЗОВАН!</>;
+};
+
+export default observer(UsersTable);
